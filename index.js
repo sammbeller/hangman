ca528dd3c56b71cd1a3060cb40d488ad30981dad
sbeller <sammbeller@gmail.com>
2018-01-18 12:21:21 -0500

    some initial comments

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const uuidV4 = require('uuid/v4'); // Random uuid
 
+/**
+ * An object mapping from uuids to an object with the following shape
+ * {string} word - The word to be guessed
+ * {int} timestamp - A timestamp generated using Date.now(), used to clean up old games
+ * {int} incorrectGuesses - The number of incorrect guesses for this game so far
+ * {boolean[]} markedLetters - An array of booleans of length equal to word indicating whether or not a letter has been correctly guessed
+ *  true if correctly guessed
+ *  false if not yet guessed
+ */
 const games = {};
 
+/**
+ * Initialize a new game object with the given uuid
+ *
+ * @param {string} uuid - A uuid to associate with a given game
+ */
 const startGame = (uuid) => {
   console.log("Starting game with uuid " + uuid);
   games[uuid] = {
