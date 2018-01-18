1c9c0f3c30e8959044df4eedd5efd30adc2e2595
sbeller <sammbeller@gmail.com>
2018-01-18 11:21:52 -0500

    generate uuid on /index and redirect to game

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
-const uuid = require('uuid/v4'); // Random uuid
+const uuidV4 = require('uuid/v4'); // Random uuid
+
+const games = {};
+
+const startGame = (uuid) => {
+  console.log("Starting game with uuid " + uuid);
+  games[uuid] = {
+    word: 'word'
+  };
+};
 
 
-app.get('/', (req, res) => res.send(uuid()));
+app.get('/', (req, res) => {
+  const uuid = uuidV4();
+  startGame(uuid);
+  res.redirect('/'+uuid);
+});
+app.get('/:uuid', (req, res) => res.send(games[req.params.uuid]));
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
